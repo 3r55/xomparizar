7834d9be6f9d2485a1a94605c8b458e4fa18ece3
Glitch (purple-iodized-stream) <none>
2020-12-26 19:02:22 +0000

    💿🐌 Checkpoint ./README.md:23877998/230 ./bot.js:23877998/187

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-
-
->BLACK BOT
-
-
->BLACK bot open soruce 
-
-
-https://cdn.glitch.com/cde9688f-8823-4f2f-874e-c3c4a5b9469f%2F971E11BE-9C0D-4E30-B56C-B5085392B930.gif?v=1609007137827
+BLACK BOT BY BLACK JACK
--- bot.js
+++ bot.js
@@ -391,7 +391,7 @@ client.on("guildMemberAdd", member => {
         .addField(' سێرڤەر', `${member.guild.name}`,true)
         .addField('کاتی جۆینکردنت', member.guild.joinedAt ,)
         .addField("کاتی دروستکردنی ئەکاونتەکەت✅:", member.user.createdAt ,)
-        .setImage("https://media.discordapp.net/attachments/777510905041911819/792459601193992202/image0.gif")
+        .setImage("https://media.discordapp.net/attachments/765236779225055252/792462641980375070/20201226_214229.gif")
         .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
      welcomer.send({embed:heroo});
       }})
