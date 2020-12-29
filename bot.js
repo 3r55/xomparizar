29bbb767405981f4cbe2d71d31f372651e206d3a
Glitch (cuddly-acidic-walker) <none>
2020-12-29 08:00:49 +0000

    👄🎓 Checkpoint ./bot.js:17969920/6788

--- bot.js
+++ bot.js
@@ -1907,6 +1907,75 @@ client.on('message', message => {
 
 
 
+        client.on('message',async message => {
+          var room;
+          var title;
+          var duration;
+          var gMembers;
+          var filter = m => m.author.id === message.author.id;
+          if(message.content.startsWith(prefix + "giveaway")) {
+            if(!message.guild.member(message.author).hasPermission('MANAGE_GUILD')) return message.channel.send(':heavy_multiplication_x:| **ببورە رۆلت نیە**');
+            message.channel.send(`:eight_pointed_black_star:| **ناوی چەنالەکە دیاری بکە**`).then(msgg => {
+              message.channel.awaitMessages(filter, {
+                max: 1,
+                time: 20000,
+                errors: ['time']
+              }).then(collected => {
+                let room = message.guild.channels.find('name', collected.first().content);
+                if(!room) return message.channel.send(':heavy_multiplication_x:| **لم اقدر على ايجاد الروم المطلوب**');
+                room = collected.first().content;
+                collected.first().delete();
+                msgg.edit(':eight_pointed_black_star:| **اكتب مدة القيف اواي بالدقائق , مثال : 60**').then(msg => {
+                  message.channel.awaitMessages(filter, {
+                    max: 1,
+                    time: 20000,
+                    errors: ['time']
+                  }).then(collected => {
+                    if(isNaN(collected.first().content)) return message.channel.send(':heavy_multiplication_x:| **يجب عليك ان تحدد وقت زمني صحيح.. ``يجب عليك اعادة كتابة الامر``**');
+                    duration = collected.first().content * 60000;
+                    collected.first().delete();
+                    msgg.edit(':eight_pointed_black_star:| **واخيرا اكتب على ماذا تريد القيف اواي**').then(msg => {
+                      message.channel.awaitMessages(filter, {
+                        max: 1,
+                        time: 20000,
+                        errors: ['time']
+                      }).then(collected => {
+                        title = collected.first().content;
+                        collected.first().delete();
+                        try {
+                          let giveEmbed = new Discord.RichEmbed()
+                          .setAuthor(message.guild.name, message.guild.iconURL)
+                          .setTitle(title)
+                          .setDescription(`المدة : ${duration / 60000} کات`)
+                          .setFooter(message.author.username, message.author.avatarURL);
+                          message.guild.channels.find('name', room).send(giveEmbed).then(m => {
+                             let re = m.react('🎉');
+                             setTimeout(() => {
+                               let users = m.reactions.get("🎉").users;
+                               let list = users.array().filter(u => u.id !== m.author.id);
+                               let gFilter = list[Math.floor(Math.random() * list.length) + 0];
+                                 if(users.size === 1) gFilter = '**لم يتم التحديد**';
+                               let endEmbed = new Discord.RichEmbed()
+                               .setAuthor(message.author.username, message.author.avatarURL)
+                               .setTitle(title)
+                               .addField('انتهى القيف اواي !',`الفائز هو : ${gFilter}`)
+                               .setFooter(message.guild.name, message.guild.iconURL);
+                               m.edit(endEmbed);
+                             },duration);
+                           });
+                          msgg.edit(`:heavy_check_mark:| **تم اعداد القيف اواي**`);
+                        } catch(e) {
+                          msgg.edit(`:heavy_multiplication_x:| **لم اقدر على اعداد القيف اواي بسبب نقص الخصائص**`);
+                          console.log(e);
+                        }
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          }
+        });
 
 ////////////////mrfix
 
@@ -1952,6 +2021,70 @@ message.channel.send(`**<@${tomute.id}> has been unmuted!**`);
 break;
 }
 });
+
+client.on('message',async msg => {
+        if(msg.channel.type === "dm") return;
+     if(msg.author.bot) return;
+     var p = "*";
+     if(msg.content.startsWith(p + "setstats")) {
+     if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **لا تملك رتبه لذلك**');
+     if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
+     var ggg= msg.guild.createChannel('SERVER STATS', 'category').then(kk => {
+     
+              var ccc =msg.guild.createChannel('SERVER STATS', 'voice').then(al => {
+                   var aa =msg.guild.createChannel('SERVER STATS', 'voice').then(alp => {
+                      var aaa =msg.guild.createChannel('SERVER STATS', 'voice').then(alph => {
+              
+          al.setParent(kk);
+          alp.setParent(kk);
+          alph.setParent(kk);
+    
+        al.overwritePermissions(msg.guild.id, {
+         CONNECT: false,
+         SPEAK: false
+       });
+        alp.overwritePermissions(msg.guild.id, {
+         CONNECT: false,
+         SPEAK: false
+       });
+        alph.overwritePermissions(msg.guild.id, {
+         CONNECT: false,
+         SPEAK: false
+       });
+     setInterval(() => {
+         var currentTime = new Date(),
+   hours = currentTime.getHours() + 0 ,
+   minutes = currentTime.getMinutes(),
+   Seconds = currentTime.getSeconds(),
+   Year = currentTime.getFullYear(),
+   Month = currentTime.getMonth() + 1,
+   Dat = currentTime.getDate()
+   if (minutes < 10) {
+   minutes = "0" + minutes;
+   }
+   var suffix = "AM";
+   if (hours >= 12) {
+   suffix = "PM";
+   hours = hours - 12;
+   }
+   if (hours == 0) {
+   hours = 12;
+   }
+       
+        al.setName(`All member : ${msg.guild.members.filter(m => m.voiceChannel).size} `);
+         alp.setName(`Member :[${hours} : ${minutes} : ${Seconds} ${suffix}]`);
+           alph.setName(`Bot : [${Year} - ${Month} - ${Dat} `);
+    },1000);
+                      })
+
+                   })
+              })
+     })
+
+     }
+
+   });
+
 
 
 client.on("message", message => {
