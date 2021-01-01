e7aca2bc08b4caf5aedf7b3346a784f9450f206e
Glitch (cuddly-acidic-walker) <none>
2021-01-01 03:36:35 +0000

    🎍🎠 Checkpoint ./bot.js:17969920/9354

--- bot.js
+++ bot.js
@@ -183,25 +183,102 @@ client.on("message", message => {
 });
 
  
-              
-    
+const sWlc = {}
+client.on('message', message => {
+var prefix = "-";
+if(message.channel.type === "dm") return;
+if(message.author.bot) return;
+  if(!sWlc[message.guild.id]) sWlc[message.guild.id] = {
+    channel: "welcome"
+}
+const channel = sWlc[message.guild.id].channel
+  if (message.content.startsWith(prefix + "setwelcomer")) {
+    if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
+    let newChannel = message.content.split(' ').slice(1).join(" ")
+    if(!newChannel) return message.reply(`**${prefix}setwelcomer <channel name>**`)
+    sWlc[message.guild.id].channel = newChannel
+    message.channel.send(`**${message.guild.name}'s channel has been changed to ${newChannel}**`);
+  }
+});
 
-
-            
-        
-
-        
-        
-
-
-
-
+client.on("guildMemberAdd", member => {
+      if(!sWlc[member.guild.id]) sWlc[member.guild.id] = {
+    channel: "welcome"
+  }
+  const channel = sWlc[member.guild.id].channel
+    const sChannel = sWlc[member.guild.id].channel
+    let welcomer = member.guild.channels.find('name', sChannel);
+    let memberavatar = member.user.avatarURL
+      if (!welcomer) return;
+      if(welcomer) {
+         moment.locale('ar-ly');
+         var h = member.user;
+        let heroo = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(h.avatarURL)
+        .setAuthor(h.username,h.avatarURL)
+        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
+         .addField(': تاريخ دخولك السيرفر',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} \n\`\`${moment(member.joinedAt).startOf(' ').fromNow()}\`\``, true)      
+         .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
+     welcomer.send({embed:heroo});          
+         
+      var Canvas = require('canvas')
+      var jimp = require('jimp')
+     const w = ['swlc.png'];
       
-
+              let Image = Canvas.Image,
+                  canvas = new Canvas(557, 241),
+                  ctx = canvas.getContext('2d');
   
+              fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+                  if (err) return console.log(err)
+                  let BG = Canvas.Image;
+                  let ground = new Image;
+                  ground.src = Background;
+                  ctx.drawImage(ground, 0, 0, 557, 241);
+      
+      })
+      
+                      let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
+                      jimp.read(url, (err, ava) => {
+                          if (err) return console.log(err);
+                          ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                              if (err) return console.log(err);
+      
+                                    ctx.font = '30px Arial Bold';
+                              ctx.fontSize = '20px';
+                              ctx.fillStyle = "#FFFFFF";
+                                ctx.fillText(member.user.username, 245, 150);
+                              
+                              //NAMEً
+                              ctx.font = '30px Arial';
+                              ctx.fontSize = '28px';
+                              ctx.fillStyle = "#FFFFFF";
+      ctx.fillText(`Welcome To Server`, 245, 80);
+    
+                              //AVATARً
+                              let Avatar = Canvas.Image;
+                              let ava = new Avatar;
+                              ava.src = buf;
+                              ctx.beginPath();
+                 ctx.arc(120.8, 120.5, 112.3, 0, Math.PI*2, true);
+                   ctx.closePath();
+                   
+                                 ctx.clip();
 
+                        ctx.drawImage(ava, 7, 8, 227, 225);
+                              ctx.closePath();
 
-        
+                            
+    welcomer.sendFile(canvas.toBuffer())
+      
+      
+      
+      })
+      })
+      
+      }
+      });        
 
             
     
@@ -924,83 +1001,134 @@ client.on("message", async message => {
   }
 });
 
-
-let anti = JSON.parse(fs.readFileSync("./antigreff.json", "UTF8"));
-let config = JSON.parse(fs.readFileSync("./config.json", "UTF8"));
+let anti = JSON.parse(fs.readFileSync("./antigrefff.json", "UTF8"));
+let config = JSON.parse(fs.readFileSync("./server.json", "UTF8"));
 client.on("message", message => {
-    if (!message.channel.guild) return;
-    let user = anti[message.guild.id + message.author.id]
-    let num = message.content.split(" ").slice(2).join(" ");
-    if (!anti[message.guild.id + message.author.id]) anti[message.guild.id + message.author.id] = {
-        actions: 0
+  if (!message.channel.guild) return;
+  let user = anti[message.guild.id + message.author.id];
+  let num = message.content
+    .split(" ")
+    .slice(2)
+    .join(" ");
+  if (!anti[message.guild.id + message.author.id])
+    anti[message.guild.id + message.author.id] = {
+      actions: 0
+    };
+  if (!config[message.guild.id])
+    config[message.guild.id] = {
+      banLimit: 3,
+      chaDelLimit: 3,
+      chaCrLimit: 3,
+      roleDelLimit: 3,
+      kickLimits: 3,
+      roleCrLimits: 3,
+      time: 30
+    };
+  if (message.content.startsWith(prefix + "anti")) {
+    if (message.author.id !== message.guild.owner.user.id){
+      let embeeed = new Discord.RichEmbed()
+      .setTitle("Protection+")
+      .setDescription("**COMMAND SECURITY JUST FOR OWNER SHIP**")
+      .setColor("9e1c36");
+      return message.channel.send(embeeed);
     }
-    if (!config[message.guild.id]) config[message.guild.id] = {
-        banLimit: 3,
-        chaDelLimit: 3,
-        roleDelLimit: 3,
-        kickLimits: 3,
-        roleCrLimits: 3,
-        time: 30
+    {
+      let arkam = new Discord.RichEmbed()
+      .setTitle("Protection+")
+      .setDescription("**Send number**")
+      .setColor("9e1c36");
+ 
+      { let arkam2 = new Discord.RichEmbed()
+      .setTitle("Protection+")
+      .setDescription("**Send number**")
+      .setColor("9e1c36");
+ 
+      if (message.content.startsWith(prefix + "anti ban")) {
+      if (!num) return message.channel.send(arkam);
+      if (isNaN(num)) return message.channel.send(arkam2);
+      config[message.guild.id].banLimit = num
+       { let banLimit1 = new Discord.RichEmbed()
+      .setTitle("Protection+")
+      .setDescription(`Change to : **${config[message.guild.id].banLimit}**`)
+      .setColor("9e1c36");
+        message.channel.send(banLimit1);}
     }
-    if (message.content.startsWith(prefix + "anti")) {
-      if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('**Bbwra adminstatort nya**')
-        
-        if (message.content.startsWith(prefix + "anti ban")) {
-            if (!num) return message.channel.send("**⇏ | Type Number**");
-            if (isNaN(num)) return message.channel.send("**⇏ | Number Only**");
-            config[message.guild.id].banLimit = num;
-            message.channel.send(`**⇏ | Done It's Now : ${config[message.guild.id].banLimit} **`)
-        }
-        if (message.content.startsWith(prefix + "anti kick")) {
-            if (!num) return message.channel.send("**⇏ | Type Number**");
-            if (isNaN(num)) return message.channel.send("**⇏ | Numbers Only **");
-            config[message.guild.id].kickLimits = num;
-            message.channel.send(`**⇏ | Done It's Now : ${config[message.guild.id].kickLimits}**`)
-        }
-        if (message.content.startsWith(prefix + "anti roleD")) {
-            if (!num) return message.channel.send("**⇏ | Type Number**");
-            if (isNaN(num)) return message.channel.send("**Numbers Only**");
-            config[message.guild.id].roleDelLimit = num;
-            message.channel.send(`**⇏ | Done It's Now : ${config[message.guild.id].roleDelLimit}**`)
-        }
-        if (message.content.startsWith(prefix + "anti roleC")) {
-            if (!num) return message.channel.send("**⇏ | Type Number**");
-            if (isNaN(num)) return message.channel.send("**⇏ | Numbers Only**");
-            config[message.guild.id].roleCrLimits = num;
-            message.channel.send(`**⇏ | Done It's Now : ${config[message.guild.id].roleCrLimits}**`)
-        }
-        if (message.content.startsWith(prefix + "anti channelD")) {
-            if (!num) return message.channel.send("**⇏ | Type Number **");
-            if (isNaN(num)) return message.channel.send("**⇏ | Numbers Only **");
-            config[message.guild.id].chaDelLimit = num;
-            message.channel.send(`**⇏ | Done It's Now : ${config[message.guild.id].chaDelLimit}**`)
-        }
-       if (message.content.startsWith(prefix + "anti channelC")) {
-      if (!num)
-        return message.channel.send(
-          "**❌ | Type A `Number` After Commands .**"
-        ); 
-      if (isNaN(num))
-        return message.channel.send("**❌ | Only Type `Number` .**");
-      config[message.guild.id].chaCrLimit = num;
-      message.channel.send(
-        `**✔️ | Changed \`Channel Create\` To : ${config[message.guild.id].chaCrLimit}**`
-      );
+    if (message.content.startsWith(prefix + "anti kick")) {
+      if (!num) return message.channel.send(arkam);
+      if (isNaN(num)) return message.channel.send(arkam2);
+      config[message.guild.id].kickLimits = num
+      let embedddd = new Discord.RichEmbed()
+      .setTitle("Protection+")
+      .setDescription(`Change to : **${config[message.guild.id].kickLimits}**`)
+      .setColor("9e1c36");
+      message.channel.send(embedddd);
     }
-        if (message.content.startsWith(prefix + "anti time")) {
-            if (!num) return message.channel.send("**⇏ | Type Number **");
-            if (isNaN(num)) return message.channel.send("**⇏ | Numbers Only**");
-            config[message.guild.id].time = num;
-            message.channel.send(`**⇏ | Done It's Now : ${config[message.guild.id].time}**`)
-        }
-        fs.writeFile("./config.json", JSON.stringify(config, null, 2), function (e) {
-            if (e) throw e;
-        });
-        fs.writeFile("./antigreff.json", JSON.stringify(anti, null, 2), function (e) {
-            if (e) throw e;
-        });
+    if (message.content.startsWith(prefix + "anti roleD")) {
+      if (!num) return message.channel.send(arkam);
+      if (isNaN(num)) return message.channel.send(arkam2);
+      config[message.guild.id].roleDelLimit = num
+      let embeddddddddd = new Discord.RichEmbed()
+      .setTitle("Protection+")
+      .setDescription(`Change to : **${config[message.guild.id].roleDelLimit}**`)
+      .setColor("9e1c36");
+      message.channel.send(embeddddddddd);
     }
+    if (message.content.startsWith(prefix + "anti roleC")) {
+      if (!num) return message.channel.send(arkam);
+      if (isNaN(num)) return message.channel.send(arkam2);
+      config[message.guild.id].roleCrLimits = num
+      let embeed = new Discord.RichEmbed()
+      .setTitle("Protection+")
+      .setDescription(`Change to : **${config[message.guild.id].roleCrLimits}**`)
+      .setColor("9e1c36");
+      message.channel.send(embeed);
+ 
+    }
+    if (message.content.startsWith(prefix + "anti channelD")) {
+      if (!num) return message.channel.send(arkam);
+      if (isNaN(num)) return message.channel.send(arkam2);
+      config[message.guild.id].chaDelLimit = num
+            let embeeed = new Discord.RichEmbed()
+      .setTitle("Protection+")
+      .setDescription(`Change to : **${config[message.guild.id].chaDelLimit}**`)
+      .setColor("9e1c36");
+      message.channel.send(embeeed);
+ 
+ 
+    }
+    if (message.content.startsWith(prefix + "anti channelC")) {
+      if (!num) return message.channel.send(arkam);
+      if (isNaN(num)) return message.channel.send(arkam2);
+      config[message.guild.id].chaCrLimit = num
+      let embd = new Discord.RichEmbed()
+      .setTitle("Protection+")
+      .setDescription(`Change to : **${config[message.guild.id].chaCrLimit}**`)
+      .setColor("9e1c36");
+      message.channel.send(embd);
+ 
+    }
+    if (message.content.startsWith(prefix + "anti time")) {
+      if (!num) return message.channel.send(arkam);
+      if (isNaN(num)) return message.channel.send(arkam2);
+      config[message.guild.id].time = num
+            let emb = new Discord.RichEmbed()
+      .setTitle("Protection+")
+      .setDescription(`Change to : **${config[message.guild.id].time}**`)
+      .setColor("9e1c36");
+      message.channel.send(emb);
+    }
+  }
+  fs.writeFile("./config.json", JSON.stringify(config, null, 2), function(e) {
+    if (e) throw e;
+  });
+  fs.writeFile("./antigreff.json", JSON.stringify(anti, null, 2), function(e) {
+    if (e) throw e;
+  });
+    }}
 });
+
+
+        
 client.on("channelDelete", async channel => {
     const entry1 = await channel.guild.fetchAuditLogs({
         type: 'CHANNEL_DELETE'
