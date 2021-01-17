4e79ff2b62d559f52782a6b39708830336cf14b1
Glitch (cuddly-acidic-walker) <none>
2021-01-17 08:10:17 +0000

    👦☺️ Checkpoint ./index.js:17969920/832

--- index.js
+++ index.js
@@ -54,6 +54,31 @@ client.on("message", async msg => {
   }
 });
 
+client.on('message', message => {
+if(message.content.startsWith(prefix + 'qfl')) {
+(!message.guild.member(message.author).hasPermission("MANAGE_CHANNELS"))
+message.channel.updateOverwrite(message.channel.id, { SEND_MESSAGES: false, VIEW_CHANNEL: false});
+const embed = new Discord.RichEmbed()
+ .setColor("#00FF00")
+ .addField("Locked channel")
+
+ message.channel.send(embed)
+
+}
+})
+client.on('message', message => {
+if(message.content.startsWith(prefix + 'unlock')){
+(!message.guild.member(message.author).hasPermission("MANAGE_CHANNELS")) 
+message.channel.updateOverwrite(message.channel.id, { SEND_MESSAGES: true, VIEW_CHANNEL: true});
+const embed = new Discord.RichEmbed()
+ .setColor("#00FF00")
+ .addField("unlock channel")
+
+ message.channel.send(embed)
+
+}
+})
+
 client.on("message", message => {
   let command = message.content.split(" ")[0];
   if (command == prefix + "unban") {
